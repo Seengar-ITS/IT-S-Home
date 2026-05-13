f068c17cfababf8c02cbf5ea79d09b1246ec48e6
Seengar <seengar237@gmail.com>
2026-05-13 15:33:51 +0500

    fix: correct invalid import identifier My-bookings

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Home from './pages/Home'
 import Book from './pages/Book'
-import My-bookings from './pages/My-bookings'
+import MyBookings from './pages/My-bookings'
 import Providers from './pages/Providers'
 export default function App() {
   return (
@@ -9,7 +9,7 @@ export default function App() {
       <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/book" element={<Book />} />
-          <Route path="/my-bookings" element={<My-bookings />} />
+          <Route path="/my-bookings" element={<MyBookings />} />
           <Route path="/providers" element={<Providers />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
